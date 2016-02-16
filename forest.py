8e5720f871d3041c71debf89d004a36283c28ee1
bjornwolf <bjornwolf@migrant-fleet.home>
2016-02-16 18:12:55 +0100

    Exchanged the set in BFS for a list to get O(1) used-checks

--- forest.py
+++ forest.py
@@ -1,30 +1,33 @@
 from graph import Graph, FlatGraph
 from collections import deque, Counter
 
+
 class Forest(object):
-    def __init__(self, vertices_map, min_graph_elems = 500):
+    def __init__(self, vertices_map, min_graph_elems=500):
         self.vertices_map = vertices_map
         self.min_graph_elems = min_graph_elems
 
     def build_connected_components(self):
-        used = set()
+        used = [False] * (max(self.vertices_map.keys()) + 1)
         result = []
 
         for v in self.vertices_map:
-            if v in used:
+            if used[v]:
                 continue
-            
-            connected_component_keys = []            
+
+            connected_component_keys = []
             bfs_q = deque([v])
             while len(bfs_q) != 0:
                 pv = bfs_q.popleft()
-                used.add(pv)
+                used[pv] = True
                 connected_component_keys.append(pv)
                 for nv in self.vertices_map[pv]['similars']:
-                    if nv not in used:
+                    if nv >= len(used):
+                        print nv, len(used)
+                    if not used[nv]:
                         bfs_q.append(nv)
-        
-            result.append(connected_component_keys)            
+
+            result.append(connected_component_keys)
         return result
 
     def build_forest(self, connected_components):
@@ -33,8 +36,8 @@ class Forest(object):
             graph_type = Graph
             if len(connected_component) < self.min_graph_elems:
                 graph_type = FlatGraph
-            self.elements.append(graph_type({ k: self.vertices_map[k] for k in
-                connected_component }))
+            self.elements.append(graph_type({k: self.vertices_map[k] for k in
+                                 connected_component}))
 
     def elements_count(self):
         return len(self.elements)
