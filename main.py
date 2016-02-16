ec657b6e6aef285c23bca8c09108c449188cfd8f
Marcin <killavus@gmail.com>
2016-02-16 22:44:22 +0100

    More trees removed

--- main.py
+++ main.py
@@ -11,18 +11,13 @@ def run():
     print config_dict
     data_location = config_dict['data_location']
     vertices_map = load_data(data_location)
-    # print "* Data loaded (%d entries)" % (len(data))
-    # vertices_map = vertices_map_from(data)
     broken, unequal = fix_similarity_symmetry(vertices_map)
     print "* Fixed similarity relation symmetry (%d unidirected, %d unequal)" % (broken, unequal)
 
     print "* Vertices map generated"
     _, deleted = purge_invalid_vertices(vertices_map)
     print "* Cleaned up vertices map (deleted %d isolated vertices)" % (deleted)
-
-    
-#similars_hist(vertices_map)
-
+ 
     if 'min_elems' in config_dict:
         forest = Forest(vertices_map, min_graph_elems=config_dict['min_elems'])
     else:
