18e7d36ae334935c9365f92b344ba889496e97f1
bjornwolf <bjornwolf@migrant-fleet.home>
2016-02-18 17:00:31 +0100

    User generator

--- main_rec.py
+++ main_rec.py
@@ -1,7 +1,6 @@
 import yaml
 from recommender import Recommender
 import sys
-import random
 import json
 
 
@@ -12,17 +11,18 @@ def prettyprint_song(config_dict, fname):
     print json_dict['artist'], ' -- ', json_dict['title']
     print json_dict['tags']
 
-# @profile
+
 def run():
     config_dict = yaml.load(open(sys.argv[1], 'r'))
     uniq = config_dict['uniq_map_file']
     runiq = config_dict['runiq_map_file']
     path = config_dict['pickle_dir']
-    
     rec = Recommender(path, uniq, runiq)
     ch = []
     user = rec.generate_user()
     ch = rec.recommend(user, n=5)
+    for fname in user:
+        prettyprint_song(config_dict, fname)
     print user, ' --> ', ch
     # prettyprint_song(config_dict, name)
     print '-->'
